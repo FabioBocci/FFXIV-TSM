6b2eee156bf8f8496de14bb7ca660cf1750dc43e
fabiobocci <fabiobocci99@gmail.com>
2024-08-08 12:21:41 +0200

    aggiunto modulo per mettere interruzioni ai cron

---
+++ cron_inactivity_period/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "Inactivity Periods for Cron Jobs",
+    "version": "16.0.1.0.0",
+    "author": "FabioBocci",
+    "website": "https://github.com/FabioBocci",
+    "license": "AGPL-3",
+    "category": "Tools",
+    "depends": ["base"],
+    "data": ["security/ir.model.access.csv", "views/ir_cron.xml"],
+    "images": ["static/description/ir_cron_form.png"],
+    "installable": True,
+}
